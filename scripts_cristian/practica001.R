c1e42a4bc77b28b179c4d814f646793b58fe4a2e
Cristian Alexander Salazar Atalaya <cristiansalazaratalaya@hotmail.com>
2020-08-27 01:14:40 -0500

    practica 001

---
+++ scripts_cristian/practica001.R
@@ -0,0 +1,53 @@
+#Esta linea no funciona por ahora
+#lm(y|z)
+
+installed.packages()
+
+choose(5,2)
+choose(5,3)
+
+cos(60)
+cospi(60)
+cos(60*pi/180)
+
+
+arcos(0.5)
+acos(0.5)*180/pi
+
+
+print(sqrt(2),22)
+round(sqrt(2),22)
+floor(sqrt(2))
+ceiling(sqrt(2))
+
+
+round((sqrt(2)^2)-2,2)
+#MAXIMO 16 CIFRAS 
+
+print(pi+5000000000000000,16)
+round(pi+5,40)
+
+#round redondea al par mas cercano a menso que sea 1
+x = pi^2
+
+
+c <- sqrt(5)
+
+pi * sqrt(5) -> asd
+
+
+edad = "30"
+
+
+doble = function(x){x+2}
+doble(5)
+
+ls() #listar todos los elemtnos creados
+rm(asd) #borrar una variable
+ls()
+#rm(list=ls()) #borra todas las variables
+
+
+
+
+
